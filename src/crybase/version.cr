c8a0c22bdd8fb19ef42090b2a275cc25850062e2
Anar Jafarov <anar.k.jafarov@gmail.com>
2026-05-22 01:47:23 +0100

    bump v0.0.2
    Signed-off-by: Anar Jafarov <anar.k.jafarov@gmail.com>

--- src/crybase/version.cr
+++ src/crybase/version.cr
@@ -2,7 +2,7 @@ module CryBase
   # Shard version, kept in sync with `shard.yml`.
   #
   # ```
-  # CryBase::VERSION # => "0.0.1"
+  # CryBase::VERSION # => "0.0.2"
   # ```
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
