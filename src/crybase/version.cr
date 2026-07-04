b2faf3bd35da64ae78bc61966585509921d81fc3
Anar Jafarov <anar.k.jafarov@gmail.com>
2026-07-04 13:12:51 +0100

    fix version
    Signed-off-by: Anar Jafarov <anar.k.jafarov@gmail.com>

--- src/crybase/version.cr
+++ src/crybase/version.cr
@@ -2,7 +2,7 @@ module CryBase
   # Shard version, kept in sync with `shard.yml`.
   #
   # ```
-  # CryBase::VERSION # => "0.0.2"
+  # CryBase::VERSION # => "0.0.4"
   # ```
-  VERSION = "0.0.2"
+  VERSION = "0.0.4"
 end
